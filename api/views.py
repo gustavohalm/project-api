7172a751163b0b17b81c887bf9f1f7f0895bbf8b
gustavo <contato@gustavohalm.com>
2020-04-12 19:48:04 -0300

    Project ViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from . import serializers, models
 
-# Create your views here.
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ProjectSerializer
+    queryset = models.Project.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+
+        return Response(serializer.data)
