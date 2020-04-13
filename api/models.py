a0590ec033a0bb684846757e0f5eb269112fb938
gustavo <contato@gustavohalm.com>
2020-04-12 22:33:16 -0300

    max_lenght in project name

--- api/models.py
+++ api/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Project(models.Model):
-    name = models.CharField(null=False, blank=False)
+    name = models.CharField(null=False, blank=False, max_length=128)
     init_date = models.DateField(null=False, blank=False)
     finished_date = models.DateField(null=False, blank=False)
---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from .views import ProjectViewSet
+from django.urls import path
+
+router = routers.DefaultRouter()
+router.register('project/', ProjectViewSet)
+
+urlpatterns = [
+    path('/', router.urls),
+]
